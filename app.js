58913a92a554416b08dcd8fe8141f5b17d2ce8df
Shawn Page <shawn.page@gmail.com>
2015-12-28 14:29:31 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by bspage on 12/28/15.
+ */
+
+class Channel extends React.Component{
+    render() {
+        return (
+            <li>Channel name</li>
+        )
+    }
+}
+
+ReactDOM.render(<Channel />, document.getElementById('app'));
+
